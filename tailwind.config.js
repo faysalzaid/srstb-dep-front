df622cf1defe0baf3cfbcc851557139d5c6e381d
Faysal Ali Hussein <61043420+faysalzaid@users.noreply.github.com>
2023-03-12 16:54:36 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const windmill = require('@windmill/react-ui/config')
 
 module.exports = windmill({
-  purge: ['src/**/*.js'],
+  purge: ['src/**/*.js','src/**/*.jsx', 'src/**/*.html'],
   theme: {
     extend: {
       fontFamily: {
