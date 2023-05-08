e50f55118de3918cf11c7036b6b3611ba28d2f57
Jabir Omar <jabiromar400@gmail.com>
2023-05-08 11:25:21 +0300

    recharts

---
+++ src/recharts/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  { name: "Jan", now: 4000, Last_Year: 2400 },
+  { name: "Feb", now: 3000, Last_Year: 1398 },
+  { name: "Mar", now: 2000, Last_Year: 4000 },
+  { name: "Apr", now: 2780, Last_Year: 3908 },
+  { name: "May", now: 1890, Last_Year: 4800 },
+  { name: "Jun", now: 2390, Last_Year: 3800 },
+  { name: "Jul", now: 3490, Last_Year: 4300 },
+  { name: "Aug", now: 3000, Last_Year: 1398 },
+  { name: "Sep", now: 2000, Last_Year: 4000 },
+  { name: "Oct", now: 2780, Last_Year: 3908 },
+  { name: "Nov", now: 1890, Last_Year: 4800 },
+  { name: "Dec", now: 4000, Last_Year: 2400 },
+];
