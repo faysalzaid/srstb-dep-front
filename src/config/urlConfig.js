6c15fad5c8a33400f32f2355fe36d72d53eb223b
faysalzaid <faysal0503070@gmail.com>
2023-05-23 08:44:46 +0300

    added the empl fileupload file present in empl list

--- src/config/urlConfig.js
+++ src/config/urlConfig.js
@@ -1,3 +1,4 @@
 export const url = 'http://localhost:4000'
-export const bidUrl = 'http://localhost:4000/docs'
-export const bidUserPic = 'http://localhost:4000/images'
+export const bidUrl = `${url}/docs`
+export const bidUserPic = `${url}/images`
+export const EmplFileDownload = `${url}/excel.xlsx`
