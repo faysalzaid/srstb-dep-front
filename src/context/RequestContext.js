64a67fdcdf05b62f4cddbb1f93121cbded36196d
Jabir Omar <jabiromar400@gmail.com>
2023-06-20 07:32:10 +0300

    request contexts

---
+++ src/context/RequestContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const FuelContext = createContext(null);
+
+export const MoneyContext = createContext(null);
+
+export const ContractContext = createContext(null);
+
+export const CarRentalContext = createContext(null);
+
+export const EquipmentRentalContext = createContext(null);
