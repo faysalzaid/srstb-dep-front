4d79dc096f1b55003ee171fff5cf572b86e4d0eb
faysalzaid <faysal0503070@gmail.com>
2023-07-03 16:17:33 +0300

    new updates

--- src/config/urlConfig.js
+++ src/config/urlConfig.js
@@ -1,4 +1,4 @@
-export const url = 'http://localhost:4000'
+export const url = 'https://cicd.amiigotech.com'
 export const bidUrl = `${url}/docs`
 export const bidUserPic = `${url}/images`
 export const EmplFileDownload = `${url}/excel.xlsx`
